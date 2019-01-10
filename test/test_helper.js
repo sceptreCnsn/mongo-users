cc2bde33bc13282f7740078c8fed9067056d952b
sceptreCnsn <cansin.memis@clb.lcl>
2019-01-10 18:26:54 +0300

    commit

--- test/test_helper.js
+++ test/test_helper.js
@@ -5,3 +5,10 @@ mongoose.connection
   .on("error", error => {
     console.warn("Error", error);
   });
+
+beforeEach((done)=>{
+    mongoose.connection.collections.users.drop(() => {
+        // Ready to run the next test
+        done();
+    });
+});
